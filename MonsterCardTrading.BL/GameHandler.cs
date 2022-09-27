98a1f61a38a79842de369e087314b5f983f69f49
SirPoz <nicolas@pozdena.at>
2022-09-27 17:21:12 +0200

    correct Project layout

---
+++ MonsterCardTrading.BL/GameHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using MonsterCardTrading.Model;
+
+namespace MonsterCardTrading.BL
+{
+    internal class GameHandler
+    {
+        public GameHandler()
+        {
+
+        }
+
+        public void StartBattle(User player1, User player2)
+        {
+
+        }
+    }
+}
---
+++ MonsterCardTrading.Model/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterCardTrading.Model
+{
+    public class User
+    {
+        private string username;
+        private string password;
+        public User(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+        
+    }
+}
