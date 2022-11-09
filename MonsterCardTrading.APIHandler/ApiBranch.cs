2425d6dfad3e6f0ce4ec164fd3c6268b1a22e359
SirPoz <nicolas@pozdena.at>
2022-11-09 15:13:12 +0100

    HTTPServer added, Start of Attributes

---
+++ MonsterCardTrading.APIHandler/ApiBranch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+
+namespace MonsterCardTrading.APIHandler
+{
+    [AttributeUsage(HTTPMethod | APIPath)]
+    internal class ApiBranch : Attribute
+    {
+        private const object APIPath = VALUE;
+        private const enum HTTPMethod = {
+            POST, PUT, GET, DELETE
+        };
+};
+    }
+}
